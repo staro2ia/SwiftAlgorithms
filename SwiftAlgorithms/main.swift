9e891222ff4810ee45be4da892f08f669dac0be3
Pavel <pasha.st@gmail.com>
2017-10-01 20:36:52 +1000

    Initial Commit

---
+++ SwiftAlgorithms/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftAlgorithms
+//
+//  Created by Павел Тимош on 01.10.2017.
+//  Copyright © 2017 Павел Тимош. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
