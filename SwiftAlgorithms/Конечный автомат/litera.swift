0a1372b4156b622adfd6f54af0f95a53d584f7d1
Pavel <pasha.st@gmail.com>
2017-12-09 22:58:23 +1000

    Litera сделана перечислением. Добавлена функция для определения класса символа.

--- SwiftAlgorithms/Конечный автомат/litera.swift
+++ SwiftAlgorithms/Конечный автомат/litera.swift
@@ -8,8 +8,6 @@
 
 import Foundation
 
-
-
 /// Типы входных сигналов автомата
 ///
 /// - EOF: конец файла
@@ -20,12 +18,75 @@ import Foundation
 enum InputType {
     case EOF
     case space
-    case letter
-    case digit
+    case letter (Character)
+    case digit (Character)
+    case noAlp
+    
+    func getInputType(char: Character) -> InputType {
+        switch char {
+        case "\0":
+            return .EOF
+        case "a"..."z", "A"..."Z":
+            return .letter(char)
+        case "0"..."9":
+            return .digit(char)
+        case " ", "\t", "\n":
+            return .space
+        default:
+            return .noAlp
+        }
+    }
+}
+
+enum Litera {
+    case EOF
+    case space
+    case letter (Character)
+    case digit (Character)
     case noAlp
 }
 
-/// Литера текста
-struct Litera {
-    <#fields#>
+func getInputType(char: Character?) -> Litera {
+    if let ch = char {
+        switch ch {
+//        case "\0":
+//            return .EOF
+        case "a"..."z", "A"..."Z":
+            return .letter(ch)
+        case "0"..."9":
+            return .digit(ch)
+        case " ", "\t", "\n":
+            return .space
+        default:
+            return .noAlp
+        }
+    }
+    return .EOF
 }
+
+/// Литера текста
+//struct Litera {
+//    var char: Character
+//    var synTerm: InputType
+//
+//    mutating func getLiteral(character ch: Character) -> InputType {
+//        char = ch
+//
+//    }
+//
+//}
+
+//fileprivate func isAlpha(char: Character) -> Bool {
+//    if  case "a"..."z" = char {
+//        return true
+//    }
+//    return false
+//}
+//
+//fileprivate func isDigit(char: Character) -> Bool {
+//    if case "0"..."9" = char {
+//        return true
+//    }
+//    return false
+//}
+
